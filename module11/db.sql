8f6ffb38a20c9aff082aa69ff392eb5bf97d4035
ernesdumani <ernesdumani@gmail.com>
2024-12-04 17:45:13 +0100

    ernesdumani
    added functions and sql

---
+++ module11/db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE user_dashboard;
+ USE user_dashboard;
+
+ CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+ );
+
+ INSERT INTO users(username, email)
+ VALUES
+ ("user1", "user1@gmail.com"),
+  ("user2", "user2@gmail.com"),
+   ("user3", "user3@gmail.com"),
+    ("user4", "user4@gmail.com"),
